d3fe47c13758f9cded964474bcea98c67fe9f6d7
John Dorsey <34581345+IamJohnDorsey@users.noreply.github.com>
2021-10-20 11:36:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -49,4 +49,7 @@ app.get("/*", (_, res) => {
 });
 
 // Listen on port 3000
-app.listen(3000, () => console.log("Application running on port 3000"));
+//app.listen(3000, () => console.log("Application running on port 3000"));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
